5e8e469aab225844ce29fddeaff386d818c8f2b0
h1divp <71522316+h1divp@users.noreply.github.com>
2026-04-28 17:29:32 -0400

    feat: implement add_item, add_customer, db lifecycle methods

--- db_handler.py
+++ db_handler.py
@@ -1,3 +1,4 @@
+from re import S
 from MARIADB_CREDS import DB_CONFIG
 from mariadb import connect
 from models.RentalHistory import RentalHistory
@@ -20,16 +21,119 @@ def add_item(new_item: Item = None):
     new_item - An Item object containing a new item to be inserted into the DB in the item table.
         new_item and its attributes will never be None.
     """
-    raise NotImplementedError("you must implement this function")
 
+    cur.execute("SELECT MAX(i_item_sk) + 1 FROM item")
+    sk = cur.fetchone()[0]
+        
+    start_date = f"{new_item.start_year}-01-01" # TODO: is there any other way which we can figure out the date?
 
+    cur.execute("""
+                INSERT INTO item (
+                   i_item_sk,
+                   i_item_id,
+                   i_rec_start_date,
+                   i_product_name,
+                   i_brand,
+                   i_class,
+                   i_category,
+                   i_manufact,
+                   i_current_price,
+                   i_num_owned
+                ) VALUES (?,?,?,?,?,?,?,?,?,?)
+                """,
+                (
+                    sk,
+                    new_item.item_id,
+                    start_date,
+                    new_item.product_name,
+                    new_item.brand,
+                    None,
+                    new_item.category,
+                    new_item.manufact,
+                    new_item.current_price,
+                    new_item.num_owned
+                )
+            )
+
+    return
+
+def parse_address(address: str):
+    parts = [p.strip() for p in address.split(",")]
+
+    street = parts[0].split()
+    street_number = street[0]
+    street_name = street[1]
+
+    city = parts[1]
+
+    state_zip = parts[2].split() 
+    state = state_zip[0]
+    zip = state_zip[1]
+
+    print(street_number, street_name, city, state, zip)
+
+    return street_number, street_name, city, state, zip
+   
 
 def add_customer(new_customer: Customer = None):
     """
     new_customer - A Customer object containing a new customer to be inserted into the DB in the customer table.
         new_customer and its attributes will never be None.
     """
-    raise NotImplementedError("you must implement this function")
+    # raise NotImplementedError("you must implement this function")
+
+    street_number, street_name, city, state, zip = parse_address(new_customer.address)
+
+    cur.execute("SELECT MAX(ca_address_sk) + 1 FROM customer_address")
+    address_sk = cur.fetchone()[0]
+
+    cur.execute("""
+                INSERT INTO customer_address (
+                    ca_address_sk,
+                    ca_street_number,
+                    ca_street_name,
+                    ca_city,
+                    ca_state,
+                    ca_zip
+                ) VALUES (?,?,?,?,?,?)
+                """, (
+                    address_sk,
+                    street_number,
+                    street_name,
+                    city,
+                    state,
+                    zip
+                )
+            )
+
+
+    cur.execute("SELECT MAX(c_customer_sk) + 1 FROM customer")
+    sk = cur.fetchone()[0]
+
+    name = new_customer.name.split(" ")
+    first_name = name[0]
+    last_name = name[1]
+
+    cur.execute("""
+                INSERT INTO customer (
+                    c_customer_sk,
+                    c_customer_id,
+                    c_first_name,
+                    c_last_name,
+                    c_email_address,
+                    c_current_addr_sk
+                ) VALUES (?,?,?,?,?,?)
+                """,
+                (
+                    sk,
+                    new_customer.customer_id,
+                    first_name,
+                    last_name,
+                    new_customer.email,
+                    address_sk
+                )
+            )
+    return
 
 
 def edit_customer(original_customer_id: str = None, new_customer: Customer = None):
@@ -152,13 +256,16 @@ def save_changes():
     """
     Commits all changes made to the db.
     """
-    raise NotImplementedError("you must implement this function")
+    conn.commit()
 
 
 def close_connection():
     """
     Closes the cursor and connection.
     """
-    raise NotImplementedError("you must implement this function")
+    try:
+        cur.close()
+    finally:
+        conn.close()
 
 
